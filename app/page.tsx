e7a200db965a1321804b3fb983fa02917653b0a1
YT <yitbarek.ayni@gmail.com>
2024-10-08 14:16:30 -0500

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -400,72 +400,6 @@ return (
                                 <img
                                   alt=""
                                   loading="lazy"
-                                  width="32"
-                                  height="32"
-                                  decoding="async"
-                                  data-nimg="1"
-                                  className="h-7 w-7"
-                                  src="/_next/static/media/planetaria.ecd81ade.svg"
-                                />
-                              </div>
-                              <dl className="flex flex-auto flex-wrap gap-x-2">
-                                <dt className="sr-only">Company</dt>
-                                <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                                  Comcast
-                                </dd>
-                                <dt className="sr-only">Role</dt>
-                                <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-                                  Software Engineer
-                                </dd>
-                                <dt className="sr-only">Date</dt>
-                                <dd
-                                  className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-                                  aria-label="2019 until Present"
-                                >
-                                  <time dateTime="2019">2023</time>
-                                  <span aria-hidden="true">—</span>
-                                  <time dateTime="2024">2024</time>
-                                </dd>
-                              </dl>
-                            </li>
-                            <li className="flex gap-4">
-                              <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                                <img
-                                  alt=""
-                                  loading="lazy"
-                                  width="28"
-                                  height="28"
-                                  decoding="async"
-                                  data-nimg="1"
-                                  className="h-7 w-7"
-                                  src="/_next/static/media/airbnb.b4000690.svg"
-                                />
-                              </div>
-                              <dl className="flex flex-auto flex-wrap gap-x-2">
-                                <dt className="sr-only">Company</dt>
-                                <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                                  United Health
-                                </dd>
-                                <dt className="sr-only">Role</dt>
-                                <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-                                  Software Engineer
-                                </dd>
-                                <dt className="sr-only">Date</dt>
-                                <dd
-                                  className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-                                  aria-label="2014 until 2019"
-                                >
-                                  <time dateTime="2014">2022</time>
-                                  <span aria-hidden="true">—</span>
-                                  <time dateTime="2019">2023</time>
-                                </dd>
-                              </dl>
-                            </li>
-                            <li className="flex gap-4">
-                              <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                                <img
-                                  alt=""
-                                  loading="lazy"
                                   width="28"
                                   height="28"
                                   decoding="async"
@@ -490,7 +424,7 @@ return (
                                 >
                                   <time dateTime="2011">2019</time>
                                   <span aria-hidden="true">—</span>
-                                  <time dateTime="2014">2020</time>
+                                  <time dateTime="2014">2023</time>
                                 </dd>
                               </dl>
                             </li>
