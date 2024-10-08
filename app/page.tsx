2b00751c41e260b2b819040b0c4b26bd279897f1
YT <yitbarek.ayni@gmail.com>
2024-10-08 14:35:20 -0500

    pic

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ return (
                       <Image
                         alt=""
                         fetchPriority="high"
+                        quality={100}
                         width="100"
                         height="100"
                         className="rounded-full inline-block"
@@ -466,7 +467,7 @@ return (
                             className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
                             href="#"
                           >
-                            Download CV
+                            Download Resume
                             <svg
                               viewBox="0 0 16 16"
                               fill="none"
