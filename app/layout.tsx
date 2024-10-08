693a11bae36848a32cc9a1772c573a8b967d4b11
YT <yitbarek.ayni@gmail.com>
2024-10-08 14:21:49 -0500

    minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,14 @@ export default function RootLayout({
                           ></div>
                           <nav className="pointer-events-auto hidden md:block">
                             <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+                            <li>
+                                <a
+                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                  href="/"
+                                >
+                                  Home
+                                </a>
+                              </li>
                               <li>
                                 <a
                                   className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
