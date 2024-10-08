05fd723dd282164c6fa4ba43e157abb4727608a7
YT <yitbarek.ayni@gmail.com>
2024-10-08 16:28:15 -0500

    upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -98,6 +98,9 @@ return (
               
               </div>
             </div>
+            <h5 className="font-bold tracking-tight text-zinc-600 sm:text-2xl dark:text-zinc-400 mt-8">
+               Project Highlight
+            </h5>
             <div className="mt-16 sm:mt-20">
               <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
                 <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2">
