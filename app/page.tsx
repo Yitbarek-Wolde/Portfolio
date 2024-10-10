5dadec4640df8900207ee048be250619020126e5
YT <yitbarek.ayni@gmail.com>
2024-10-10 18:26:48 -0500

    description update

--- app/page.tsx
+++ app/page.tsx
@@ -270,6 +270,37 @@ return (
                             <span className="ml-3">Work</span>
                           </h2>
                           <ol className="mt-6 space-y-4">
+                          <li className="flex gap-4">
+                              <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                                {/* <img
+                                  alt=""
+                                  loading="lazy"
+                                  width="28"
+                                  height="28"
+                                  decoding="async"
+                                  data-nimg="1"
+                                  className="h-7 w-7"
+                                  src="/_next/static/media/starbucks.4a5bd050.svg"
+                                /> */}
+                              </div>
+                              <dl className="flex flex-auto flex-wrap gap-x-2">
+                                <dt className="sr-only">Company</dt>
+                                <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                                  Maharishi International University, Iowa
+                                </dd>
+                                <dt className="sr-only">Role</dt>
+                                <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+                                  Teaching Assitant
+                                </dd>
+                                <dt className="sr-only">Date</dt>
+                                <dd
+                                  className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+                                  aria-label="2023"
+                                >
+                                <time dateTime="2023">Nov 2023</time>
+                                </dd>
+                              </dl>
+                            </li>
                             <li className="flex gap-4">
                               <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                                 {/* <img
