ef697b1acc996649f31a9003af98f8f4fccc6de4
YT <yitbarek.ayni@gmail.com>
2024-10-09 19:15:22 -0500

    navBar responsiveness fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Head from "next/head";
 import "./globals.css";
+import Navbar from "./navBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -59,75 +60,7 @@ export default function RootLayout({
                     <div className="mx-auto max-w-2xl lg:max-w-5xl">
                       <div className="relative flex gap-4">
                         <div className="flex flex-1"></div>
-                        <div className="flex flex-1 justify-end md:justify-center">
-                          <div
-                            className="pointer-events-auto md:hidden"
-                            data-headlessui-state=""
-                          >
-                            <button
-                              className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
-                              type="button"
-                              aria-expanded="false"
-                              data-headlessui-state=""
-                            >
-                              Menu
-                              <svg
-                                viewBox="0 0 8 6"
-                                aria-hidden="true"
-                                className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400"
-                              >
-                                <path
-                                  d="M1.75 1.75 4 4.25l2.25-2.5"
-                                  fill="none"
-                                  stroke-width="1.5"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                ></path>
-                              </svg>
-                            </button>
-                          </div>
-                          <div
-                            hidden
-                            className="position:fixed;top:1px;left:1px;width:1px;height:0;padding:0;margin:-1px;overflow:hidden;clip:rect(0, 0, 0, 0);white-space:nowrap;border-width:0;display:none"
-                          ></div>
-                          <nav className="pointer-events-auto hidden md:block">
-                            <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-                            <li>
-                                <a
-                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                                  href="/"
-                                >
-                                  Home
-                                </a>
-                              </li>
-                              <li>
-                                <a
-                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                                  href="/#about"
-                                >
-                                  About
-                                </a>
-                              </li>
-                              <li>
-                                <a
-                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                                  href="/articles"
-                                >
-                                  Articles
-                                </a>
-                              </li>
-                              <li>
-                                <a
-                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                                  href="/projects"
-                                >
-                                  Projects
-                                </a>
-                              </li>
-                              
-                            </ul>
-                          </nav>
-                        </div>
+                        <Navbar />
                         <div className="flex justify-end md:flex-1">
                           <div className="pointer-events-auto"></div>
                         </div>
@@ -143,7 +76,11 @@ export default function RootLayout({
               <div className="sm:px-8 mt-9">
                 <div className="mx-auto w-full max-w-7xl lg:px-8">
                   <div className="relative px-4 sm:px-8 lg:px-12">
+
+                  {/* pages render */}
                     {children}
+
+
                   </div>
                 </div>
               </div>
