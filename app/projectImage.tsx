953a0560dd9bb834ac730ace898510ddc2ee6450
YT <yitbarek.ayni@gmail.com>
2024-10-10 01:05:19 -0500

    version 1.5.0 ready for technical article page

--- app/projectImage.tsx
+++ app/projectImage.tsx
@@ -4,54 +4,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import projectData from "./projectData.json";
 import "swiper/swiper-bundle.css";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import { memo } from "react";
 
 type projectType = {
   src: string;
   title: string;
   description: string;
   date: string;
-  link:string;
+  link: string;
 };
-
-const ProjectList = memo((projects: any) => (
-  <SwiperSlide>
-    <div className="flex items-start gap-4">
-      <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2 inline-block">
-        <img
-          alt=""
-          className="absolute inset-0 h-full w-full object-cover"
-          src={projects.src}
-        />
-      </div>
-
-      <article className="group relative flex flex-col items-start inline-block">
-        <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-          <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-          <a href="/articles/crafting-a-design-system-for-a-multiplanetary-future">
-            <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-            <span className="relative z-10">{projects.title}</span>
-          </a>
-        </h2>
-        <time
-          className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
-          dateTime="2022-09-05"
-        >
-          <span
-            className="absolute inset-y-0 left-0 flex items-center"
-            aria-hidden="true"
-          >
-            <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
-          </span>
-          {projects.date}
-        </time>
-        <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-          {projects.description}
-        </p>
-      </article>
-    </div>
-  </SwiperSlide>
-));
 
 const ImageSlider = () => {
   const projects: projectType[] = projectData;
@@ -59,71 +19,68 @@ const ImageSlider = () => {
   return (
     <div className="mt-16 sm:mt-20">
       <Swiper
-        autoplay={ {delay: 3000} }
+        autoplay={{ delay: 3000 }}
         spaceBetween={10}
-        slidesPerView={1} 
+        slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
-        loop={true} 
+        loop={true}
         modules={[Navigation, Pagination, Autoplay]}
       >
         <>
-          {projects.map((projects, index) => (
-          //  <ProjectList key={index} project={project} />
-          <SwiperSlide>
-          <div className="flex items-start gap-4">
-            <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2 inline-block">
-              <img
-                alt=""
-                className="absolute inset-0 h-full w-full object-cover"
-                src={projects.src}
-              />
-            </div>
-      
-            <article className="group relative flex flex-col items-start inline-block">
-              <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                {/* <a href="/articles/crafting-a-design-system-for-a-multiplanetary-future"> 
+          {projects.map((projects) => (
+            //  <ProjectList key={index} project={project} />
+            <SwiperSlide>
+              <div className="flex items-start gap-4">
+                <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2 inline-block">
+                  <img
+                    alt=""
+                    className="absolute inset-0 h-full w-full object-cover"
+                    src={projects.src}
+                  />
+                </div>
+
+                <article className="group relative flex flex-col items-start">
+                  <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
+                    {/* <a href="/articles/crafting-a-design-system-for-a-multiplanetary-future"> 
                   <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
                   
-                 </a> */}<span className="relative z-10">{projects.title}</span>
-              </h2>
-              <time
-                className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
-                dateTime="2022-09-05"
-              >
-                <span
-                  className="absolute inset-y-0 left-0 flex items-center"
-                  aria-hidden="true"
-                >
-                  <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
-                </span>
-                {projects.date}
-              </time>
-              <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                {projects.description}
-              </p>
-              <br/>
-             <a
-                          className="group -m-1 p-1 z-0"
-                          href={projects.link}
-                        >
-                          <svg
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z"
-                            ></path>
-                          </svg>
-                        </a>
-            </article>
-            
-          </div>
-        </SwiperSlide>
+                 </a> */}
+                    <span className="relative z-10">{projects.title}</span>
+                  </h2>
+                  <time
+                    className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+                    dateTime="2022-09-05"
+                  >
+                    <span
+                      className="absolute inset-y-0 left-0 flex items-center"
+                      aria-hidden="true"
+                    >
+                      <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+                    </span>
+                    {projects.date}
+                  </time>
+                  <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                    {projects.description}
+                  </p>
+                  <br />
+                  <a className="group -m-1 p-1 z-0" href={projects.link}>
+                    <svg
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                      className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z"
+                      ></path>
+                    </svg>
+                  </a>
+                </article>
+              </div>
+            </SwiperSlide>
           ))}
         </>
       </Swiper>
