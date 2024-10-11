ffeb6bfa55cc3cd01804228b179b6d6d757e7429
YT <yitbarek.ayni@gmail.com>
2024-10-11 00:31:47 -0500

    migration

--- app/projectImage.tsx
+++ app/projectImage.tsx
@@ -33,10 +33,11 @@ const ImageSlider = () => {
             <SwiperSlide key={index}>
               <div className="flex items-start gap-4">
                 <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2 inline-block">
-                  <Image
+                  <img
                   alt={"project" + index} 
                     className="absolute inset-0 h-full w-full object-cover"
                     src={projects.src}
+                    
                   />
                 </div>
 
